c8345d575488fb418cc5dd734ac17e3e6aaf8d63
alexsanderfr <alexsanderfrancorosa@gmail.com>
2019-04-03 15:00:15 -0300

    feat: Add index reducer

---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+import comments from './comments'
+import posts from './posts'
+import categories from './categories'
+
+export default combineReducers({
+    categories,
+    posts,
+    comments
+})
