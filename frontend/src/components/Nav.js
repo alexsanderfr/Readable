ce5da056f1ed2a98c50487ff2a5bca079637169d
alexsanderfr <alexsanderfrancorosa@gmail.com>
2019-04-09 20:38:00 -0300

    feat: Add bootstrap

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -3,19 +3,21 @@ import { NavLink } from 'react-router-dom'
 
 export default function Nav() {
     return (
-        <nav className='nav'>
-            <ul>
-                <li>
-                    <NavLink to='/' exact activeClassName='active'>
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/categories' exact activeClassName='active'>
-                        Categories
-                    </NavLink>
-                </li>
-            </ul>
+        <nav className="navbar navbar-default">
+            <div className="container-fluid">
+                <ul className="nav navbar-nav">
+                    <li>
+                        <NavLink to='/' exact activeClassName='active'>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/categories' exact activeClassName='active'>
+                            Categories
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
