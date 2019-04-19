548a286a0fa232ed7f74b6a21cd7f943e2d8525c
alexsanderfr <alexsanderfrancorosa@gmail.com>
2019-04-19 12:34:10 -0300

    fix: Fix comment count

--- frontend/src/reducers/posts.js
+++ frontend/src/reducers/posts.js
@@ -2,10 +2,13 @@ import {
     RECEIVE_POSTS, RECEIVE_POST, RECEIVE_POSTS_BY_CATEGORY, ADD_POST,
     VOTE_POST, DISABLE_POST, EDIT_POST
 } from '../actions/posts'
+import {
+    ADD_COMMENT, DISABLE_COMMENT
+} from '../actions/comments'
 
 export default function posts(state = {}, action) {
     switch (action.type) {
-        case RECEIVE_POSTS: 
+        case RECEIVE_POSTS:
             let postsObject = {}
             action.posts.forEach(element => {
                 postsObject[element.id] = element
@@ -47,6 +50,22 @@ export default function posts(state = {}, action) {
             return {
                 ...state,
                 [action.post.id]: action.post
+            }
+        case ADD_COMMENT:
+            return {
+                ...state,
+                [action.comment.parentId]: {
+                    ...state[action.comment.parentId],
+                    commentCount: state[action.comment.parentId].commentCount + 1
+                }
+            }
+        case DISABLE_COMMENT:
+            return {
+                ...state,
+                [action.comment.parentId]: {
+                    ...state[action.comment.parentId],
+                    commentCount: state[action.comment.parentId].commentCount - 1
+                }
             }
         default:
             return state
